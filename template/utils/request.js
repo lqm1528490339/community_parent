7ee043f1cda2a3eb41df02996166dec8dc723511
one <1528490339@qq.com>
2019-04-13 22:05:31 +0800

    修改数据库配置文件

--- template/utils/request.js
+++ template/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {getUser} from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
-    baseURL: 'http://192.168.184.133:7300/mock/5af314a4c612520d0d7650c7', // api的base_url
+    baseURL: 'http://119.23.205.248:7300/mock/5ca992a5f83de243736b276d', // api的base_url
     timeout: 30000, // 请求超时时间
     headers: { 'Authorization':  "Bearer "+getUser().token }
   })
